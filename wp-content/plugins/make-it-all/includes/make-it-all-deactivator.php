aaedba5c8a7a1450f0e7a79838869f32f0a61957
TobyMellor <tobymulberry@hotmail.com>
2018-03-02 13:04:29 +0000

    WP plugin boilerplate

---
+++ wp-content/plugins/make-it-all/includes/make-it-all-deactivator.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    make-it-all
+ * @subpackage make-it-all/includes
+ * @author     Toby Mellor <me@tobymellor.co.uk>
+ */
+class MakeItAllDeactivator {
+	/**
+	 * Deactivates the Make It All plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+		//
+	}
+}
