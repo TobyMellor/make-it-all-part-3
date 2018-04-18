8ee0dd8bcfd74ac26e5f4c4cc10d5eede538e73c
TobyMellor <tobymulberry@hotmail.com>
2018-04-18 20:54:55 +0100

    Spaces > Tabs in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,11 +8,11 @@
  */
 
 let mix = require("laravel-mix"),
-    fs  = require("fs");
+	fs  = require("fs");
 
 mix.setPublicPath('./');
 mix.options({
-    processCssUrls: false
+	processCssUrls: false
 });
 
 // Lambda functions for performing small string operations
@@ -24,65 +24,65 @@ let resources = (baseFolder, name) => folder(baseFolder, name) + name;
 let output = (baseFolder, name) => "wordpress/wp-content/themes/make-it-all/" + folder(baseFolder, name).replace("sass", "css") + name
 
 function compileFrontend() {
-    frontendResources = name => resources("resources/frontend/", name); // baseFolder always "frontend" for frontend files
-    frontendOutput    = name => output("frontend/", name); // set output directory
+	frontendResources = name => resources("resources/frontend/", name); // baseFolder always "frontend" for frontend files
+	frontendOutput    = name => output("frontend/", name); // set output directory
 
-    // List of pages' folders to iterate through contents to get JS and CSS for compilation (must be in order)
-    let pages = ["tickets", "staff", "hardware", "software", "metrics", "problem_types", "settings"];
+	// List of pages' folders to iterate through contents to get JS and CSS for compilation (must be in order)
+	let pages = ["tickets", "staff", "hardware", "software", "metrics", "problem_types", "settings"];
 
-    // Compile common JS across many pages
-    mix.js(["main.js", "pages/DynamicPage.js", "pages/Manager.js"].map(frontendResources), frontendOutput("main.js"));
+	// Compile common JS across many pages
+	mix.js(["main.js", "pages/DynamicPage.js", "pages/Manager.js"].map(frontendResources), frontendOutput("main.js"));
 
-    // Compile JS and CSS for each individual page
-    for (let page of pages) {
-        mix.js(frontendResources("pages/" + page + "/" + page + ".js"), frontendOutput("pages/" + page + ".js"));
-        mix.sass(frontendResources("pages/" + page + ".scss"), frontendOutput("pages/" + page + ".css"));
-    }
+	// Compile JS and CSS for each individual page
+	for (let page of pages) {
+		mix.js(frontendResources("pages/" + page + "/" + page + ".js"), frontendOutput("pages/" + page + ".js"));
+		mix.sass(frontendResources("pages/" + page + ".scss"), frontendOutput("pages/" + page + ".css"));
+	}
 
-    // Compile common SCSS for use on all pages
-    // Not used since each page CSS imports the main CSS for single file minification
-    // Can be re-enabled if there is a use case for purely using the main CSS
-    //mix.sass("resources/assets/sass/app.scss", "public/css");
+	// Compile common SCSS for use on all pages
+	// Not used since each page CSS imports the main CSS for single file minification
+	// Can be re-enabled if there is a use case for purely using the main CSS
+	//mix.sass("resources/assets/sass/app.scss", "public/css");
 
-    compileVendors(frontendResources, frontendOutput);
+	compileVendors(frontendResources, frontendOutput);
 }
 
 function compileBackend() {
-    backendResources = name => resources("resources/backend/", name); // baseFolder always "backend" for backend files
-    backendOutput    = name => output("backend/", name); // set output directory
+	backendResources = name => resources("resources/backend/", name); // baseFolder always "backend" for backend files
+	backendOutput    = name => output("backend/", name); // set output directory
 
-    mix.js(backendResources("main.js"), backendOutput("main.js"));
-    mix.sass(backendResources("main.scss"), backendOutput("main.css"))
+	mix.js(backendResources("main.js"), backendOutput("main.js"));
+	mix.sass(backendResources("main.scss"), backendOutput("main.css"))
 
 
-    let pages   = ["ticket", "hardware"],
-        actions = ["read", "create", "update"];
+	let pages   = ["ticket", "hardware"],
+		actions = ["read", "create", "update"];
 
-    for (let page of pages) {
-        // common styles/scripts across actions, e.g. tickets/tickets.js
-        mix.js(backendResources(page + "/" + page + ".js"), backendOutput(page + "/" + page + ".js"));
-        mix.sass(backendResources(page + "/" + page + ".scss"), backendOutput(page + "/" + page + ".css"));
+	for (let page of pages) {
+		// common styles/scripts across actions, e.g. tickets/tickets.js
+		mix.js(backendResources(page + "/" + page + ".js"), backendOutput(page + "/" + page + ".js"));
+		mix.sass(backendResources(page + "/" + page + ".scss"), backendOutput(page + "/" + page + ".css"));
 
-        for (let action of actions) {
-            let actionPath = page + "/" + action + "_" + page + ".js";
+		for (let action of actions) {
+			let actionPath = page + "/" + action + "_" + page + ".js";
 
-            // scripts for an individual action, e.g. tickets/c.js
-            if (fs.existsSync(backendResources(actionPath))) {
-                mix.js(backendResources(actionPath), backendOutput(actionPath));
-            }
-        }
-    }
+			// scripts for an individual action, e.g. tickets/c.js
+			if (fs.existsSync(backendResources(actionPath))) {
+				mix.js(backendResources(actionPath), backendOutput(actionPath));
+			}
+		}
+	}
 
-    compileVendors(backendResources, backendOutput);
+	compileVendors(backendResources, backendOutput);
 }
 
 // Compile vendor JS and CSS
 // Get all files in vendor folder and compile into single file
 function compileVendors(resources, output) {
-    let vendor = name => resources("vendor/" + name);
+	let vendor = name => resources("vendor/" + name);
 
-    mix.js(vendor("vendor.js"), output("vendor.js"));
-    mix.sass(vendor("vendor.scss"), output("vendor.css"));
+	mix.js(vendor("vendor.js"), output("vendor.js"));
+	mix.sass(vendor("vendor.scss"), output("vendor.css"));
 }
 
 compileFrontend(); // frontend theme
@@ -91,9 +91,9 @@ compileBackend(); // plugin wp-admin dashboard theme
 // Production builds should be handled differently to development build
 // Detect production build
 if (mix.inProduction()) {
-    // Minification is handled automatically in production builds
+	// Minification is handled automatically in production builds
 } else {
-    // Produce source mappings for each of the JS and CSS files built
-    // to allow for better debugging in the web browser
-    mix.sourceMaps();
+	// Produce source mappings for each of the JS and CSS files built
+	// to allow for better debugging in the web browser
+	mix.sourceMaps();
 }
