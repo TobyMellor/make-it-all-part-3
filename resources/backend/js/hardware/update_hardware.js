171289d25831d6e010353b5d4c218def0d3b62a3
TobyMellor <tobymulberry@hotmail.com>
2018-04-18 21:16:24 +0100

    Formatting for update_hardware.js

--- resources/backend/js/hardware/update_hardware.js
+++ resources/backend/js/hardware/update_hardware.js
@@ -5,8 +5,6 @@ $(() => {
 
     addButtonListeners(this, (device ? device.type : ""), (device ? device.make : ""));
 
-
-
     let $heading = $('.mia-panel-heading'),
         $select  = $heading.find('select'),
         $img     = $heading.find('img');
@@ -16,11 +14,12 @@ $(() => {
             <option value="${device.id}">#${device.id} – ${device.serial_no}</option>
         `);
     });
-        $('input[name="hardware[id]"]').val(device ? device.id : "");
-        $('#change-hardware option[value="' + (device ? device.id : "") + '"]').prop('selected', true);
-        $('.hardware-type-select option[value="' + (device ? device.type : "") + '"]').prop('selected', true);
-        $('.hardware-make-select option[value="' + (device ? device.make : "") + '"]').prop('selected', true);
-        $('.hardware-serial').val( (device ? device.serial_no : ""));
+
+    $('input[name="hardware[id]"]').val(device ? device.id : "");
+    $('#change-hardware option[value="' + (device ? device.id : "") + '"]').prop('selected', true);
+    $('.hardware-type-select option[value="' + (device ? device.type : "") + '"]').prop('selected', true);
+    $('.hardware-make-select option[value="' + (device ? device.make : "") + '"]').prop('selected', true);
+    $('.hardware-serial').val((device ? device.serial_no : ""));
 
     $('#change-hardware').change(function() {
         let hardwareID = $(this).val();
@@ -28,72 +27,47 @@ $(() => {
         window.location.href = window.location.pathname + '?page=hardware_update&id=' + hardwareID;
     });
 
+    function addButtonListeners($accordian, $type, $make) {
+        let types = $($accordian).find('.add-type'),
+            makes = $($accordian).find('.add-make');
 
+        // expand type section on button press.
+        types.on('click', function() {
+            let typePanel = $(this).closest('.accordion-body').find('#type-information');
 
+            if (typePanel.hasClass('expanded')) {
+                typePanel.removeClass('expanded');
 
-    function addButtonListeners($accordian, $type, $make){
-    let types = $($accordian).find('.add-type');
-    let makes = $($accordian).find('.add-make');
+                $('.hardware-type-select option[value="' + $type + '"]').prop('selected', true);
+            } else {
+                typePanel.addClass('expanded');
 
-    //Expand type section on button press.
-    types.on('click', function(){
-        let typePanel = $(this).closest('.accordion-body').find('#type-information');
-        console.log("type clicked");
-        if(typePanel.hasClass('expanded')){
-            typePanel.removeClass('expanded');
-             $('.hardware-type-select option[value="' + $type + '"]').prop('selected', true);
+                $(this).closest('.accordion-body').find('.hardware-type-select').val('new');
+            }
+        });
 
-        } else {
-            typePanel.addClass('expanded');
-            $(this).closest('.accordion-body').find('.hardware-type-select').val('new');
+        // expand make section on button press.
+        makes.on('click', function() {
+            let makePanel = $(this).closest('.accordion-body').find('#make-information');
+            if (makePanel.hasClass('expanded')) {
+                makePanel.removeClass('expanded');
 
+                $('.hardware-make-select option[value="' + $make + '"]').prop('selected', true);
+            } else {
+                makePanel.addClass('expanded');
 
-        }
-
-
-    });
-
-
-    //Expand make section on button press.
-        makes.on('click', function(){
-        let makePanel = $(this).closest('.accordion-body').find('#make-information');
-        if(makePanel.hasClass('expanded')){
-            makePanel.removeClass('expanded');
-            $('.hardware-make-select option[value="' + $make + '"]').prop('selected', true);
-        } else {
-            makePanel.addClass('expanded');
-            $(this).closest('.accordion-body').find('.hardware-make-select').val('new');
-
-
-        }
-
-
-    });
-
-
-
-}
-
-
-
-
-
-
-
-
+                $(this).closest('.accordion-body').find('.hardware-make-select').val('new');
+            }
+        });
+    }
 
     tinyMCE.init({
         selector: 'textarea',
         branding: false,
-        setup: function (editor) {
-            editor.on('change', function () {
+        setup: function(editor) {
+            editor.on('change', function() {
                 editor.save(); // keep hidden textarea up to date
             });
         }
     });
-
-
-
-
-
 });
