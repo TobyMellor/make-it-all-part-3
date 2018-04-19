c850098b9b3dc904415cbdd066441f8e44d2c2a8
TobyMellor <tobymulberry@hotmail.com>
2018-04-19 14:48:59 +0100

    Add some comments and cleanup

--- resources/backend/js/problem_type/problem_type.js
+++ resources/backend/js/problem_type/problem_type.js
@@ -9,45 +9,31 @@ $(() => {
 	let staffManager         = window.staffManager         = new StaffManager(employees, 1, expertiseTypes, expertiseTypeStaff);
 	let expertiseTypeManager = window.expertiseTypeManager = new ExpertiseTypeManager(expertiseTypes, expertiseTypeStaff, staffManager);
 
-	if (expertiseTypeManager.expertiseTypes.length) {
-		let id = expertiseTypeManager.expertiseTypes[0].id;
-
-		expertiseTypeManager.loadExpertiseType($('.type-columns'), id);
-
-		updatePanelInfo(id)
-	} else {
-		$('.mia-panel-short .problem-type-actions > .row')
-			.hide()
-			.next()
-			.show();
-	}
+	init();
 
 	// on clicking a problem type, load and display all children of this type
 	$(document).on('click', '.type-column li', function() {
-		let id = $(this).data('expertiseTypeId');
-
-		// show the children of the selected type in the main view
-		expertiseTypeManager.loadChildrenExpertiseTypes($(this).closest('.type-columns'), $(this));
-
-		updatePanelInfo(id);
+		loadExpertiseType($(this).data('expertiseTypeId'), () => {
+			return expertiseTypeManager.loadChildrenExpertiseTypes($(this).closest('.type-columns'), $(this));
+		});
 	});
 
+	// clicking on a problem on the left side bar
 	$('.recent-problems li').click(function() {
-		let id = $(this).data('expertiseTypeId');
-
-		expertiseTypeManager.loadExpertiseType($('.type-columns'), id);
-
-		updatePanelInfo(id)
+		loadExpertiseType($(this).data('expertiseTypeId'));
 	});
 
+	// clicking the grey "Create problem type" button
 	$(document).on('click', '.type-column button:not(.button-success)', function() {
 		expertiseTypeManager.showCreateExpertiseTypeField($(this));
 	});
 
+	// keypresses when typing in a new problem type name
 	$(document).on('keyup', '.type-column input', function() {
 		expertiseTypeManager.handleCreateExpertiseTypeField($(this));
 	});
 
+	// clicking the green "Submit new problem type" button
 	$(document).on('click', '.type-column button.button-success', function() {
 		let $button            = $(this),
 			$input             = $button.siblings('input'),
@@ -105,8 +91,10 @@ $(() => {
 			});
 	});
 
+	// redirect the "Create" within button under "Problem Type Actions"
 	$('#create-problem-type').click(() => $('.type-columns .last-active').parent().next().find('button').click());
 
+	// clicking on "Rename" under "Problem Type Actions"
 	$(document).on('click', '#rename-problem-type:not(.button-success)', function() {
 		let id     = $('.type-columns li.last-active').data('expertiseTypeId'),
 			$input = $(this).prev();
@@ -121,13 +109,15 @@ $(() => {
 		$input.select();
 	});
 
+	// when typing in the rename field, disable/enable the input field accordingly
 	$(document).on('keyup', '.renaming-problem-type input', function() {
-		let $button = $(this).siblings('button');
-
 		// the name of the new ExpertiseType must be between 2 and 256
-		$button.prop('disabled', $(this).val().length <= 2 || $(this).val().length >= 256);
+		$(this)
+			.siblings('button')
+			.prop('disabled', $(this).val().length <= 2 || $(this).val().length >= 256);
 	});
 
+	// clicking on the submit button next to the rename field
 	$(document).on('click', '#rename-problem-type.button-success', function() {
 		let id                 = $('.type-columns li.last-active').data('expertiseTypeId'),
 			$input             = $(this).prev(),
@@ -161,6 +151,7 @@ $(() => {
 			});
 	});
 
+	// clicking "Delete problem type"
 	$(document).on('click', '#delete-problem-type', function() {
 		if (!confirm('Are you sure you want to delete that Problem Type?')) return;
 
@@ -200,6 +191,28 @@ $(() => {
 			});
 	});
 
+	// load the initial problem types. If not present, hide "Problem Type Actions"
+	function init() {
+		if (expertiseTypeManager.expertiseTypes.length) {
+			loadExpertiseType(expertiseTypeManager.expertiseTypes[0].id);
+		} else {
+			$('.mia-panel-short .problem-type-actions > .row')
+				.hide()
+				.next()
+				.show();
+		}
+	}
+
+	// loads the ExpertiseType based on the ID, updates the right panels
+	function loadExpertiseType(
+		id,
+		loadFunction = () => expertiseTypeManager.loadExpertiseType($('.type-columns'), id)
+	) {
+		loadFunction();
+		updatePanelInfo(id);
+	}
+
+	// functionality for the "Specialists of this Problem" right panel
 	function updatePanelInfo(expertiseTypeId) {
 		let $specialistsTbody = $('#problem-specialists'),
 			$specialistsPanel = $('.problem-specialists-panel'),
@@ -209,14 +222,17 @@ $(() => {
 			specialists       = staffManager.getSpecialistsOfSpecialism(expertiseTypeId),
 			$show, $hide;
 
+		// if this problem type has no specialists
 		if (!specialists.length) {
 			let expertiseType = expertiseTypeManager.getExpertiseType(expertiseTypeId);
 
+			// attempt to find a parent problem with specialists
 			while (!specialists.length && expertiseType.parent_id != null) {
 				expertiseType = expertiseTypeManager.getExpertiseType(expertiseType.parent_id);
 				specialists   = staffManager.getSpecialistsOfSpecialism(expertiseType.id);
 			}
 
+			// if one of the parents have specialists, show the specialists and a message
 			if (specialists.length) {
 				[$show, $hide] = [$parentSpecialist.add($table), $noSpecialists];
 
@@ -225,7 +241,7 @@ $(() => {
 				[$show, $hide] = [$noSpecialists, $parentSpecialist.add($table)];
 			}
 		} else {
-			[$show, $hide] = [$table, $noSpecialists.add($parentSpecialist)];
+			[$show, $hide] = [$table, $noSpecialists.add($parentSpecialist)]; // show the no specialists available message
 		}
 
 		$specialistsTbody.empty();
