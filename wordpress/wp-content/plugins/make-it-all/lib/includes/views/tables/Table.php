c54ce62743406a602fb9c46697cd57af79de4688
Aron <thummper@hotmail.co.uk>
2018-05-07 15:02:26 +0100

    So a comma not being a full stop broke all the tables.
    hardware/software views cannot be edited/ messed with now.

--- wordpress/wp-content/plugins/make-it-all/lib/includes/views/tables/Table.php
+++ wordpress/wp-content/plugins/make-it-all/lib/includes/views/tables/Table.php
@@ -5,7 +5,7 @@ namespace MakeItAll\Includes\Views\Tables;
 use \WP_List_Table;
 
 if (!class_exists('WP_List_Table')) {
-	require_once(ABSPATH, 'wp-admin/includes/class-wp-list-table.php');
+	require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
 }
 
 abstract class Table extends WP_List_Table {
